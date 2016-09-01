d0a2df59405df646c4947170bd3344d56672cd89
malans <marcusegues@gmail.com>
2016-09-01 23:41:43 +0200

    Change Synth Container

--- synthesizer/frontend/components/synth/synth_container.jsx
+++ synthesizer/frontend/components/synth/synth_container.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import Synth from './synth';
 import { keyPressed, keyReleased } from './../../actions/note_actions';
+import { addNotes } from './../../actions/track_actions';
 
 const mapStateToProps = (state) => ({
   notes: state.notes,
@@ -9,7 +10,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   keyPressed: key => dispatch(keyPressed(key)),
-  keyReleased: key => dispatch(keyReleased(key))
+  keyReleased: key => dispatch(keyReleased(key)),
+  addNotes: notes => dispatch(addNotes(notes))
 });
 
 export default connect(
